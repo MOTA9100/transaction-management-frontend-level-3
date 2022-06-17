eaf94c3720bd6e4c9e63dd5cf200811a09ed9393
moslem tayari <moslem_t@yahoo.com>
2022-06-17 18:15:20 +0430

    init project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/images/logo.webp" width="125" height="125" />
+    Valour
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/app.scss";
+
+</style>
